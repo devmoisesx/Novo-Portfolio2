6039b0ec18106388d5d6d4bbede60131a3270242
MoisesXavier09 <156130689+MoisesXavier09@users.noreply.github.com>
2024-03-06 02:12:37 -0300

    feat: adiciona o button

---
+++ src/Components/Button.jsx
@@ -0,0 +1,11 @@
+function Button({children, className, ...props}) {
+  return (
+    <>
+      <button className={`text-[14px] bg-gradient-to-t from-gBlack to-gGray rounded-full px-3 ${className}`} {...props}>
+        {children}
+      </button>
+    </>
+  );
+}
+
+export default Button;
