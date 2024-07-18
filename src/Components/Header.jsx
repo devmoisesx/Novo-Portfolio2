d04d9ed027f8d05a9e0fc1d79df9dcf67e0aa0aa
Moisés Xavier <secondmox91@gmail.com>
2024-07-18 14:39:25 -0300

    feat: Adicioan funcao de smooth scroll aos botos do header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,23 @@
-import { Link } from "react-router-dom";
+import { Link } from "react-scroll";
+import { Link as Link2, useLocation } from "react-router-dom";
 import Button from "./Buttons/Button";
 
 function Header() {
+  const location = useLocation();
+  // const handleScroll = (section) => {
+  //   const section2 = document.getElementById(section);
+  //   const offset = 100; // Ajuste a altura conforme necessário
+  //   const bodyRect = document.body.getBoundingClientRect().top;
+  //   const elementRect = section2.getBoundingClientRect().top;
+  //   const elementPosition = elementRect - bodyRect;
+  //   const offsetPosition = elementPosition - offset;
+
+  //   window.scrollTo({
+  //     top: 1000,
+  //     behavior: "smooth",
+  //   });
+  // };
+
   return (
     <header className="w-screen max-w-6xl p-2 md:pr-6 lg:pr-0 fixed z-50">
       <div className=" flex justify-between items-center h-[50px] px-2 py-2 rounded-full bg-[rgba(114,114,114,0.5)] backdrop-blur-md ">
@@ -12,15 +28,33 @@ function Header() {
         </Link>
 
         <nav className="hidden md:flex gap-2">
-          <Link to={"/"}>
-            <Button>Inicio</Button>
-          </Link>
-          <Link to={"/"}>
-            <Button>Inicio</Button>
-          </Link>
-          <Link to={"/"}>
-            <Button>Inicio</Button>
-          </Link>
+          {location.pathname !== "/" ? (
+            <Link2 to="/">
+              <Button>Inicio</Button>
+            </Link2>
+          ) : (
+            <Link to="homeHero" smooth={true} offset={0} duration={500}>
+              <Button>Inicio</Button>
+            </Link>
+          )}
+          {location.pathname !== "/" ? (
+            <Link2 to="/#homeProjects">
+              <Button>Projetos</Button>
+            </Link2>
+          ) : (
+            <Link to="homeProjects" smooth={true} offset={-100} duration={500}>
+              <Button>Projetos</Button>
+            </Link>
+          )}
+          {location.pathname !== "/" ? (
+            <Link2 to="/#homeAbout">
+              <Button>Sobre</Button>
+            </Link2>
+          ) : (
+            <Link to="homeAbout" smooth={true} offset={-100} duration={500}>
+              <Button>Sobre</Button>
+            </Link>
+          )}
         </nav>
         <Button
           className={
