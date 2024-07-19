c86025907eb0d1e00ba0496c667076fd21775d0d
Moisés Xavier <secondmox91@gmail.com>
2024-07-19 02:28:54 -0300

    refactor: Altera os dados dos projetos

--- src/Components/Project/ProjectsData.js
+++ src/Components/Project/ProjectsData.js
@@ -1,47 +1,58 @@
 const projectsData = [
   {
     id: 1,
-    name: "project_1",
-    stacks: ["HTML", "CSS", "JavaScript", "React"],
+    name: "criptografia_de_texto",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, consectetur? Harum, at neque! Itaque quo natus repellat provident enim iste deserunt voluptates veritatis nobis? Ab dolor dolore commodi dolores natus.",
-    technologies:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, consectetur? Harum, at neque! Itaque quo natus repellat provident enim iste deserunt voluptates veritatis nobis? Ab dolor dolore commodi dolores natus.",
+      "Ferramenta de criptografia para textos desenvolvida para o desafio da ONE Alura não apenas criptografa seus textos de forma segura, mas também os salva localmente para garantir a privacidade e a disponibilidade dos seus dados.",
     functions:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, consectetur? Harum, at neque! Itaque quo natus repellat provident enim iste deserunt voluptates veritatis nobis? Ab dolor dolore commodi dolores natus.",
+    technologies:
+      "Desenvolvida utilizando HTML para estruturação, CSS para estilização, JavaScript para funcionalidades interativas, e Git para controle de versionamento do código. Essas tecnologias foram escolhidas para garantir uma aplicação web completa, com design atraente e funcionalidades dinâmicas.",
+    stacks: ["HTML", "CSS", "JavaScript", "Git"],
   },
   {
     id: 2,
-    name: "project_2",
-    stacks: ["HTML", "CSS", "JavaScript", "React"],
+    name: "zaphira",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, consectetur? Harum, at neque! Itaque quo natus repellat provident enim iste deserunt voluptates veritatis nobis? Ab dolor dolore commodi dolores natus.",
-    technologies:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, consectetur? Harum, at neque! Itaque quo natus repellat provident enim iste deserunt voluptates veritatis nobis? Ab dolor dolore commodi dolores natus.",
+      "O site proporciona uma experiência de compra online quase completa, onde os clientes podem explorar e adquirir uma diversidade de calçados. Com um design intuitivo e funcionalidades de busca e filtragem, o site facilita a navegação e a seleção dos produtos desejados, garantindo conveniência e segurança durante todo o processo de compra.",
     functions:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, consectetur? Harum, at neque! Itaque quo natus repellat provident enim iste deserunt voluptates veritatis nobis? Ab dolor dolore commodi dolores natus.",
+      "O projeto oferece uma gama de funcionalidades para uma experiência de compra completa. Os clientes podem explorar todos os produtos disponíveis, visualizar páginas individuais de cada item para detalhes específicos, e utilizar um carrinho de compras para adicionar e remover produtos conforme desejado. Além disso, o site inclui uma página dedicada ao processo de compra, facilitando a finalização das transações de forma eficiente. Os produtos podem ser ordenados por diferentes critérios para facilitar a navegação e encontrar exatamente o que o cliente procura.",
+    technologies:
+      "Foi desenvolvido utilizando tecnologias modernas para garantir uma experiência interativa e responsiva. Utilizando HTML, CSS e Tailwind CSS para facilitar o design responsivo e eficiente. JavaScript, React e Redux foram empregados para implementar funcionalidades dinâmicas e gerenciamento de estado avançado, permitindo uma navegação fluida e interativa pelos produtos. Essas stacks combinadas proporcionam um ambiente robusto e escalável para operações comerciais online eficazes.",
+    stacks: ["HTML", "CSS", "JavaScript", "React", "Redux", "Tailwind"],
   },
   {
     id: 3,
-    name: "project_3",
-    stacks: ["HTML", "CSS", "JavaScript", "React"],
+    name: "to_do_list",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, consectetur? Harum, at neque! Itaque quo natus repellat provident enim iste deserunt voluptates veritatis nobis? Ab dolor dolore commodi dolores natus.",
-    technologies:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, consectetur? Harum, at neque! Itaque quo natus repellat provident enim iste deserunt voluptates veritatis nobis? Ab dolor dolore commodi dolores natus.",
+      "Um site de lista de tarefas simplifica o gerenciamento diário de atividades com funções básicas como adicionar, editar, marcar como concluído e excluir tarefas. É projetado para ser intuitivo, aumentando a eficiência pessoal e disponível em dispositivos móveis e desktops para conveniência total.",
     functions:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, consectetur? Harum, at neque! Itaque quo natus repellat provident enim iste deserunt voluptates veritatis nobis? Ab dolor dolore commodi dolores natus.",
+      "Ele permite criar novas tarefas, excluí-las quando necessário e marcar como concluídas. Além disso, mantém um histórico das tarefas concluídas para revisão e oferece salvamento automático para evitar perda de dados.",
+    technologies:
+      "Desenvolvida utilizando HTML para estruturação, CSS para estilização, JavaScript para funcionalidades interativas, e Git para controle de versionamento do código. Essas tecnologias foram escolhidas para garantir uma aplicação web completa, com design atraente e funcionalidades dinâmicas.",
+    stacks: ["HTML", "CSS", "JavaScript", "Git"],
   },
   {
     id: 4,
-    name: "project_4",
-    stacks: ["HTML", "CSS", "JavaScript", "React"],
+    name: "gerador_de_bordas",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, consectetur? Harum, at neque! Itaque quo natus repellat provident enim iste deserunt voluptates veritatis nobis? Ab dolor dolore commodi dolores natus.",
-    technologies:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, consectetur? Harum, at neque! Itaque quo natus repellat provident enim iste deserunt voluptates veritatis nobis? Ab dolor dolore commodi dolores natus.",
+      "A ferramenta foi criada para facilitar a modificação das bordas de quadrados, tornando o processo compreensível e acessível. Ela permite aos usuários ajustar e aplicar diferentes estilos de bordas de forma intuitiva, melhorando o design visual de interfaces e elementos gráficos de maneira eficiente.",
     functions:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, consectetur? Harum, at neque! Itaque quo natus repellat provident enim iste deserunt voluptates veritatis nobis? Ab dolor dolore commodi dolores natus.",
+      "A ferramenta permite personalizar bordas de maneira flexível, seja ajustando todas de uma vez ou individualmente. Também oferece opções entre três formas geométricas para melhor design visual, proporcionando versatilidade e controle na criação de interfaces estéticas e funcionais.",
+    technologies:
+      "Desenvolvida utilizando HTML para estruturação, CSS para estilização, JavaScript para funcionalidades interativas, e Git para controle de versionamento do código. Essas tecnologias foram escolhidas para garantir uma aplicação web completa, com design atraente e funcionalidades dinâmicas.",
+    stacks: ["HTML", "CSS", "JavaScript", "React"],
+  },
+  {
+    id: 5,
+    name: "restaurant_menu_page",
+    description:
+      "O site apresenta de forma clara e atrativa todos os pratos disponíveis para os clientes. É projetado para facilitar a navegação e ajudar os usuários a fazerem escolhas informadas sobre suas refeições.",
+    functions:
+      "O site permite aos usuários escolher entre diferentes tipos de pratos, facilitando a navegação e a seleção de refeições de acordo com suas preferências gastronômicas.",
+    technologies:
+      "Desenvolvida utilizando HTML para estruturação, CSS para estilização, JavaScript para funcionalidades interativas, e Git para controle de versionamento do código. Essas tecnologias foram escolhidas para garantir uma aplicação web completa, com design atraente e funcionalidades dinâmicas.",
+    stacks: ["HTML", "CSS", "JavaScript", "React"],
   },
 ];
 
